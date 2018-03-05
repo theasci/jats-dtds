7f7590e0823c43673c21c2cb1928733d2a6080e3
Bryan English <benglish@the-jci.org>
2018-03-05 15:12:23 -0500

    Select 1.0 jats dtd if needed

--- JatsDtds.php
+++ JatsDtds.php
@@ -3,7 +3,7 @@
 namespace theasci;
 
 class JatsDtds {
-	static public function getPath(){
-		return dirname(__FILE__) . '/JATS-journalpublishing1.dtd';
+	static public function getPath($version = '1.1d3'){
+		return dirname(__FILE__) . "/{$version}/JATS-journalpublishing1.dtd";
 	}
 }
